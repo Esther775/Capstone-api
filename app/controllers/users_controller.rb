ff61c825af83592c19526755fb295f9d0a384f4d
Esther Morris <EstherMorris775@gmail.com>
2022-08-26 16:10:03 -0400

    added user create and session create with jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      password_digest: params[:password],
+    )
+    user.save
+
+    render json: user.as_json
+  end
 end
